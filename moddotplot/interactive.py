462aa0643a760c9de0adfa4f04ac0bf1720bfcd1
alexsweeten <alex.sweeten@gmail.com>
2023-06-06 22:27:07 -0700

    Fix scaling bug. Update version

--- moddotplot/interactive.py
+++ moddotplot/interactive.py
@@ -1,3 +1,4 @@
+from venv import create
 import plotly.express as px
 from moddotplot.estimate_identity import (
     binomial_distance,
@@ -35,11 +36,11 @@ def set_zoom_levels(axis_length, sparsity_layers):
     for i in range(1, len(sparsity_layers)):
         zoom_levels[i] = round(axis_length / pow(2, i))
     return zoom_levels
-
+  
 
 def create_map(kmer_list, resolution, sparsity):
     kmers = get_mods(kmer_list, sparsity)
-    kmer_coordinates = create_coordinates(kmers, resolution)
+    kmer_coordinates = create_coordinates2(kmers, resolution)
     return kmer_coordinates
 
 
@@ -58,16 +59,43 @@ def kmer_coordinates(kmer_list, jacc, resolution, k):
 
     return jaccard_matrix
 
+def kmer_coordinates2(kmer_list1, kmer_list2, jacc, resolution, k):
+    jaccard_values = []
+    for w in kmer_list1:
+        for x in kmer_list2:
+            if jacc:
+                j_hat = poisson_distance(containment(w, x), k)
+                jaccard_values.append(j_hat)
+            else:
+                j_hat = binomial_distance(containment(w, x), k)
+                jaccard_values.append(j_hat)
 
-def run_dash(kmer_list, resolution, sparsity, k, identity, port_number, jacc):
+    jaccard_matrix = np.asanyarray(jaccard_values)
+    jaccard_matrix.shape = resolution, resolution
+
+    return jaccard_matrix
+
+def create_coordinates2(kmer_list):
+    tmp = []
+    for k in kmer_list:
+        tmp.append(set())
+        for j in k:
+            tmp[-1].add(j)
+
+    return tmp
+
+
+def run_dash(kmer_list1, resolution, sparsity, k, identity, port_number, jacc):
     # Run Dash app
     app = dash.Dash(__name__, prevent_initial_callbacks="initial_duplicate")
     current_color = COLOR_SCHEMES["Spectral"]
 
     # Create initial coordinates & figure
-    init = create_map(kmer_list, resolution, sparsity)
-    main_level = kmer_coordinates(init, False, resolution, k)
-    increments = round(len(kmer_list) / resolution)
+    init = get_mods(kmer_list1, sparsity, resolution)
+
+    new_init = create_coordinates2(init)
+    main_level = kmer_coordinates(new_init, False, resolution, k)
+    increments = round(len(kmer_list1) / resolution)
     x_ax = [i * increments for i in range(resolution)]
     fig = px.imshow(
         main_level,
@@ -144,14 +172,187 @@ def run_dash(kmer_list, resolution, sparsity, k, identity, port_number, jacc):
                     amount = x_end - x_begin
 
                     new_jaccard = []
-                    x_mers = kmer_list[x_begin:x_end]
-                    y_mers = kmer_list[y_end:y_begin]
-                    new_map_x = create_map(x_mers, resolution, sparsity)
-                    new_map_y = create_map(y_mers, resolution, sparsity)
-                    updated_jaccard_values = []
+                    x_mers = kmer_list1[x_begin:x_end]
+                    y_mers = kmer_list1[y_end:y_begin]
+                    new_map_x = get_mods(x_mers, sparsity, resolution)
+                    new_map_y = get_mods(y_mers, sparsity, resolution)
 
-                    for w in new_map_y:
-                        for q in new_map_x:
+                    x_init = create_coordinates2(new_map_x)
+                    y_init = create_coordinates2(new_map_y)
+                    updated_jaccard_values = []
+                    
+                    for w in y_init:
+                        for q in x_init:
+                            if jacc:
+                                updated_jaccard_values.append(
+                                    poisson_distance(jaccard(q, w), k)
+                                )
+                            else:
+                                updated_jaccard_values.append(
+                                    binomial_distance(containment(q, w), k)
+                                )
+
+                    new_jaccard = np.asanyarray(updated_jaccard_values)
+                    new_jaccard.shape = resolution, resolution
+                    harvest2 = new_jaccard
+                    x_ax = [
+                        relayoutData["xaxis.range[0]"] + (i * amount / resolution)
+                        for i in range(resolution)
+                    ]
+                    y_ax = [
+                        relayoutData["yaxis.range[1]"] + (i * amount / resolution)
+                        for i in range(resolution)
+                    ]
+                    new_fig = px.imshow(
+                        harvest2,
+                        x=x_ax,
+                        y=y_ax,
+                        zmin=identity / 100,
+                        zmax=1,
+                        height=1000,
+                        width=1000,
+                        color_continuous_scale=COLOR_SCHEMES["Spectral"],
+                    )
+                    current_fig = new_fig
+                    current_fig.update_layout(hovermode="x unified")
+                    current_fig.update_xaxes(showspikes=True, spikemode="across")
+                    current_fig.update_yaxes(showspikes=True, spikemode="across")
+                    current_fig.update_layout(
+                        hoverlabel=dict(
+                            bgcolor="white", font_size=16, font_family="Rockwell"
+                        )
+                    )
+                return current_fig
+        except TypeError as err:
+            # Supress this specific instance of a type error warning, since this will always occur on setup.
+            return fig
+
+        except KeyError:
+            try:
+                # This occurs when the axes are reset. Revert back to the original figure
+                if relayoutData["xaxis.autorange"]:
+                    return fig
+            except KeyError as errw:
+                if relayoutData["dragmode"] == "pan":
+                    return figure
+
+    # Callback for toggle switch
+    @app.callback(
+        Output("my-toggle-switch-output", "children"),
+        Input("my-toggle-switch", "value"),
+    )
+    def update_output(value):
+        if not value:
+            return f"Resolution lock: Off"
+        else:
+            return f"Resolution lock: On"
+
+    app.run_server(debug=True, use_reloader=False, port=port_number)
+
+def run_dash_pairwise(kmer_list1, kmer_list2, resolution, sparsity, k, identity, port_number, jacc):
+    # Run Dash app
+    print("using pairwise")
+    app = dash.Dash(__name__, prevent_initial_callbacks="initial_duplicate")
+    current_color = COLOR_SCHEMES["Spectral"]
+
+    # Create initial coordinates & figure
+    x_d = get_mods(kmer_list1, sparsity, resolution)
+    y_d = get_mods(kmer_list2, sparsity, resolution)
+
+    x_init = create_coordinates2(x_d)
+    y_init = create_coordinates2(y_d)
+
+    main_level = kmer_coordinates2(x_init, y_init, False, resolution, k)
+    x_increments = round(len(kmer_list1) / resolution)
+    y_increments = round(len(kmer_list2) / resolution)
+    x_ax = [i * x_increments for i in range(resolution)]
+    y_ax = [i * y_increments for i in range(resolution)]
+    fig = px.imshow(
+        main_level,
+        x=x_ax,
+        y=y_ax,
+        zmin=identity / 100,
+        zmax=1,
+        height=1000,
+        width=1000,
+        color_continuous_scale=current_color,
+    )
+
+    # Optional layout changes
+    fig.update_layout(hovermode="x unified")
+    fig.update_xaxes(showspikes=True, spikemode="across")
+    fig.update_yaxes(showspikes=True, spikemode="across")
+    fig.update_layout(
+        hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell")
+    )
+
+    colorscales = px.colors.named_colorscales()
+    colornames = px.colors.named_colorscales()
+
+    colorscales.insert(0, COLOR_SCHEMES["Spectral"])
+    colornames.insert(0, "Spectral")
+
+    # HTML for plot webpage
+    app.layout = html.Div(
+        [
+            daq.ToggleSwitch(
+                id="my-toggle-switch",
+                value=False,
+                color="blue",
+                style={
+                    "width": "200px",
+                    "display": "inline-block",
+                    "padding-top": "30px",
+                    "padding-left": "80px",
+                },
+            ),
+            html.Div(
+                id="my-toggle-switch-output",
+                style={
+                    "display": "inline-block",
+                    "font-family": "Rockwell, Arial, sans-serif",
+                    "padding-top": "30px",
+                },
+            ),
+            html.Div(dcc.Graph(id="dotplot", figure=fig)),
+        ]
+    )
+
+    # Callback activates when panning, zooming,
+    @app.callback(
+        Output("dotplot", "figure", allow_duplicate=True),
+        Input("dotplot", "relayoutData"),
+        Input("my-toggle-switch", "value"),
+        Input("dotplot", "figure"),
+    )
+    def update_x_timeseries(relayoutData, value, figure):
+        try:
+            if value:
+                return figure
+            elif relayoutData["xaxis.range[0]"] and not value:
+                # TODO: Handle out of bounds error
+                if (
+                    relayoutData["xaxis.range[0]"] > 0
+                    and relayoutData["yaxis.range[1]"] > 0
+                ):
+                    x_begin = round(relayoutData["xaxis.range[0]"])
+                    x_end = round(relayoutData["xaxis.range[1]"])
+                    y_begin = round(relayoutData["yaxis.range[0]"])
+                    y_end = round(relayoutData["yaxis.range[1]"])
+                    amount = x_end - x_begin
+
+                    new_jaccard = []
+                    x_mers = kmer_list1[x_begin:x_end]
+                    y_mers = kmer_list2[y_end:y_begin]
+                    new_map_x = get_mods(x_mers, sparsity, resolution)
+                    new_map_y = get_mods(y_mers, sparsity, resolution)
+
+                    x_init = create_coordinates2(new_map_x)
+                    y_init = create_coordinates2(new_map_y)
+                    updated_jaccard_values = []
+                    
+                    for w in y_init:
+                        for q in x_init:
                             if jacc:
                                 updated_jaccard_values.append(
                                     poisson_distance(jaccard(q, w), k)
--- moddotplot/moddotplot.py
+++ moddotplot/moddotplot.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-from moddotplot.parse_fasta import read_kmers_from_file, get_input_headers
-from moddotplot.estimate_identity import get_mods, partition_windows
-from moddotplot.interactive import run_dash
+from moddotplot.parse_fasta import read_kmers_from_file, get_input_headers, get_input_seq_length
+from moddotplot.estimate_identity import get_mods, partition_windows, partition_windows_limit, partition_evenly_spaced_modimizers
+from moddotplot.interactive import run_dash, run_dash_pairwise
 from moddotplot.const import ASCII_ART
 import argparse
 import math
-from moddotplot.static_plots import create_plots, paired_bed_file
+from moddotplot.static_plots import create_plots, paired_bed_file, paired_bed_file_a_vs_b
+import itertools
 
 
 def get_args_parse():
@@ -88,6 +89,12 @@ def get_args_parse():
         help="By default, histograms are evenly spaced based on the number of colors and the identity threshold. Select this argument to bin based on the frequency of observed identity values.",
     )
 
+    plot_params.add_argument(
+        "--compare",
+        action="store_true",
+        help="Create a dotplot with two different sequences."
+    )
+
     interactive_params = parser.add_argument_group("Interactive plotting commands")
 
     interactive_params.add_argument(
@@ -121,16 +128,143 @@ def get_args_parse():
 def main():
     print(ASCII_ART)
     args = get_args_parse()
-    # Validate input sequence, display warning if multiple fasta files are shown:
+    # Validate input sequence:
+    seq_list = []
     for i in args.input:
         headers = get_input_headers(i)
+        if len(headers) > 1:
+            print(f"File {i} contains multiple fasta entries: \n")
+            counter = 1
+            for j in headers:
+                print(f"{counter}) {j} \n")
+                counter += 1
+        for j in headers:
+            seq_list.append(j)
+
+    kmer_list = []
+    for i in args.input:
+        kmer_list.append(read_kmers_from_file(i, args.kmer))
+    k_list = [item for sublist in kmer_list for item in sublist]
+
+    # Set sparsity value if not determined
+    if not args.sparsity:
+        len_list = []
+        for x in k_list:
+            len_list.append(len(x))
+        args.sparsity = math.ceil(max(len_list) / 500000)
+        print(f"Using s = {args.sparsity}. \n")
+
+    if args.interactive:
+        # Modimizers computed at runtime using Dash
+
+        if len(seq_list) > 1:
+            run_dash_pairwise(
+                k_list[0],
+                k_list[1],
+                args.resolution,
+                args.sparsity,
+                args.kmer,
+                args.identity,
+                args.port,
+                False,
+            )
+        else:
+            run_dash(
+                k_list[0],
+                args.resolution,
+                args.sparsity,
+                args.kmer,
+                args.identity,
+                args.port,
+                False,
+            )
+
+    else:
+        for i in range(len(seq_list)):
+            print(f"Computing self identity matrix for {seq_list[i]}...")
+            mod_list = get_mods(k_list[i], args.sparsity, args.resolution)
+            windows = partition_evenly_spaced_modimizers(mod_list, len(k_list[i]) + args.kmer - 1, args.resolution)
+            paired_bed_file(
+                windows,
+                seq_list[i],
+                args.identity,
+                args.sparsity,
+                args.output,
+                len(k_list[i]) + args.kmer - 1,
+                args.no_plot,
+                args.kmer
+            )
+#--------------------
+        if args.compare and len(seq_list) > 1:
+            mod_list = []
+            for i in itertools.combinations(k_list, 2):
+                print(len(i[0]), len(i[1]))
+                ratio = 0
+                if len(i[0]) > len(i[1]):
+                    ratio = round(args.resolution * len(i[1]) / len(i[0]))
+                    first = get_mods(i[1], args.sparsity, ratio)
+                    second = get_mods(i[0][0:len(i[1])], args.sparsity, ratio)
+                    third = get_mods(i[0][len(i[1])+1:len(i[0])], args.identity, ratio)
+
+                    short_dict = {}
+                    large_dict = {}
+                    for w in range(ratio):
+                        start_site = w * round(len(i[1])/ratio)
+                        end_site = w * round(len(i[1])/ratio) + round(len(i[1])/ratio) - 1
+                        name = f"{start_site}-{end_site}"
+                        short_dict[name] = first[w]
+                        large_dict[name] = second[w]
+                    for x in range(args.resolution - ratio):
+                        start_site = (ratio + x) * round(len(i[1])/ratio)
+                        end_site = (ratio + x) * round(len(i[1])/ratio) + round(len(i[1])/ratio) - 1
+                        name = f"{start_site}-{end_site}"
+                        large_dict[name] = third[x]
+                    
+                    print(len(short_dict))
+                    print(len(large_dict))
+                    paired_bed_file_a_vs_b(short_dict, large_dict, "smol", "big", args.resolution, args.identity, args.kmer)
+                else:
+                    ratio = round(args.resolution * len(i[0]) / len(i[1]))
+                    first = get_mods(i[0], args.sparsity, ratio)
+                    second = get_mods(i[1][0:len(i[0])], args.sparsity, ratio)
+                    third = get_mods(i[1][len(i[0])+1:len(i[1])], args.identity, args.resolution - ratio)
+
+                    short_dict = {}
+                    large_dict = {}
+                    for w in range(ratio):
+                        start_site = w * round(len(i[0])/ratio)
+                        end_site = w * round(len(i[0])/ratio) + round(len(i[0])/ratio) - 1
+                        name = f"{start_site}-{end_site}"
+                        short_dict[name] = first[w]
+                        large_dict[name] = second[w]
+                    for x in range(args.resolution - ratio):
+                        start_site = (ratio + x) * round(len(i[0])/ratio)
+                        end_site = (ratio + x) * round(len(i[0])/ratio) + round(len(i[0])/ratio) - 1
+                        name = f"{start_site}-{end_site}"
+                        large_dict[name] = third[x]
+                    
+                    print(len(short_dict))
+                    print(len(large_dict))
+                    paired_bed_file_a_vs_b(short_dict, large_dict, "smol", "big", args.resolution, args.identity, args.kmer)
+
+            #partition_windows_limit(kmer_list, seq_length, resolution, upper_limit, real_limit):
+            
+            #new_windows.append(partition_windows_limit(mod_list[0], len_list[0], args.resolution, max(len_list), len_list[0]))
+            #new_windows.append(partition_windows_limit(mod_list[1], len_list[1], args.resolution, max(len_list), len_list[1]))
+
+            #paired_bed_file_a_vs_b(new_windows[0], new_windows[1], seq_list[0], seq_list[1], args.resolution, args.identity, args.kmer)
+
+#--------------------
+'''
+    for i in args.input:
+        
         kmer_list = read_kmers_from_file(i, args.kmer)
 
         for seq in range(len(headers)):
             if not args.sparsity:
                 # Get input sequence length
                 args.sparsity = math.ceil(len(kmer_list[seq]) / 500000)
-                print(f"Density not provided. Using d = {args.sparsity}. \n")
+                
             if args.interactive:
                 # Modimizers computed at runtime using Dash
                 # TODO: Add Jaccard coefficient as argument
@@ -145,12 +279,13 @@ def main():
                 )
             else:
                 print(f"Computing modimizers for {headers[seq]}... \n")
-                mod_list = get_mods(kmer_list[seq], args.sparsity)
+                mod_list = get_mods(kmer_list[seq], args.sparsity, args.resolution)
                 print("Modimizers done! \n")
                 print("Creating coordinates...\n")
-                windows = partition_windows(mod_list, len(kmer_list[seq]) + args.kmer - 1, args.resolution)
+                windows = partition_evenly_spaced_modimizers(mod_list, len(kmer_list[seq]) + args.kmer - 1, args.resolution)
                 print("Coordinates done! \n")
                 print("Computing identity... \n")
+
                 paired_bed_file(
                     windows,
                     headers[seq],
@@ -160,7 +295,7 @@ def main():
                     len(kmer_list[seq]) + args.kmer - 1,
                     args.no_plot,
                     args.kmer
-                )
+                )'''
 
 
 if __name__ == "__main__":
